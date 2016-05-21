f731894cf955b8554c0812b5bf22f4d8df8b0043
cliff <r2d2.hell@gmail.com>
2016-05-21 18:50:38 +0300

    Initial commit

---
+++ src/main/java/com/cliffdurden/spring/xml/helloworld/model/Message.java
@@ -0,0 +1,14 @@
+package com.cliffdurden.spring.xml.helloworld.model;
+
+public class Message {
+
+    private String messageInfo;
+
+    public String getMessageInfo() {
+        return messageInfo;
+    }
+
+    public void setMessageInfo(String messageInfo) {
+        this.messageInfo = messageInfo;
+    }
+}
---
+++ web/WEB-INF/views/hello.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello World Spring MVC</title>
+</head>
+<body>
+<h1>Spring WebMVC Xml Config hello world application</h1>
+<br/>
+<p>The message is: ${message}</p>
+</body>
+</html>
